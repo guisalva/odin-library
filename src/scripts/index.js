29db403219674564e93cce492411ccf030760597
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-06-19 11:15:13 -0300

    Add constructor and a function to add a new Book

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+let library = [];
+
+function Book(name, author) {
+  if (!new.target) {
+    throw Error("You may use 'new' operator to call the constructor");
+  }
+
+  this.id = crypto.randomUUID();
+  this.name = name;
+  this.author = author;
+}
+
+function addBookToLibrary(name, author) {
+  library.push(new Book(name, author));
+}
+
+addBookToLibrary("Harry Potter", "J.K Rolling");
+addBookToLibrary("Harry Potter 2", "J.K Rolling");
+addBookToLibrary("Harry Potter 3", "J.K Rolling");
+addBookToLibrary("Harry Potter 4", "J.K Rolling");
+
+console.log(library);
