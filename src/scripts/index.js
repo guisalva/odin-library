115f32df81a3b534d38a6e806c3601f89836c761
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-06-19 14:59:18 -0300

    add function to render books in the page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+let booksDisplay = document.querySelector(".books-display");
+
 let library = [];
 
 function Book(name, author) {
@@ -12,11 +14,38 @@ function Book(name, author) {
 
 function addBookToLibrary(name, author) {
   library.push(new Book(name, author));
+
+  renderBookCards();
+}
+
+function renderBookCards() {
+  booksDisplay.innerHTML = "";
+
+  for (let i = 0; i < library.length; i++) {
+    const book = library[i];
+
+    const bookCard = document.createElement("div");
+    const bookTitle = document.createElement("h3");
+    const bookInfo = document.createElement("ul");
+
+    bookTitle.innerText = book.name;
+
+    Object.keys(book).forEach((key) => {
+      if (key === "id" || key === "name") return;
+
+      const info = document.createElement("li");
+      info.innerText = `${key}: ${book[key]}`;
+      bookInfo.appendChild(info);
+    });
+
+    bookCard.classList.toggle("book-card");
+    bookCard.append(bookTitle, bookInfo);
+
+    booksDisplay.appendChild(bookCard);
+  }
 }
 
 addBookToLibrary("Harry Potter", "J.K Rolling");
 addBookToLibrary("Harry Potter 2", "J.K Rolling");
 addBookToLibrary("Harry Potter 3", "J.K Rolling");
 addBookToLibrary("Harry Potter 4", "J.K Rolling");
-
-console.log(library);
