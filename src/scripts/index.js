02c20853775ba86e426f68069ba7873f5d003408
Guilherme Salvador <guilherme.s.2@outlook.com>
2026-06-22 11:12:26 -0300

    Add delete and toggle read status action buttons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,10 @@
-let booksDisplay = document.querySelector(".books-display");
-
 let library = [];
 
-function Book(name, author) {
+const booksDisplay = document.querySelector(".books-display");
+const dialog = document.getElementById("book-dialog");
+const bookForm = document.getElementById("book-form");
+
+function Book(name, author, pages, read) {
   if (!new.target) {
     throw Error("You may use 'new' operator to call the constructor");
   }
@@ -10,12 +12,24 @@ function Book(name, author) {
   this.id = crypto.randomUUID();
   this.name = name;
   this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary(name, author) {
-  library.push(new Book(name, author));
+function addBookToLibrary(name, author, pages, read) {
+  library.push(new Book(name, author, pages, read));
 
   renderBookCards();
+}
+function deleteBook(id) {
+  library = library.filter((book) => book.id !== id);
+}
+function toggleBookRead(id) {
+  const book = library.find((book) => book.id === id);
+
+  if (book) {
+    book.read = !book.read;
+  }
 }
 
 function renderBookCards() {
@@ -27,6 +41,7 @@ function renderBookCards() {
     const bookCard = document.createElement("div");
     const bookTitle = document.createElement("h3");
     const bookInfo = document.createElement("ul");
+    const bookActions = document.createElement("div");
 
     bookTitle.innerText = book.name;
 
@@ -38,26 +53,66 @@ function renderBookCards() {
       bookInfo.appendChild(info);
     });
 
+    bookActions.classList.toggle("book-actions");
+    bookActions.innerHTML = `<button type="button" data-action="delete" class="delete-btn"><img src="../src/assets/trash.svg" /><button> 
+    <button type="button" data-action="toggle-read" class="btn"><img src="../src/assets/book-open-check.svg" /><button>`;
+
     bookCard.classList.toggle("book-card");
-    bookCard.append(bookTitle, bookInfo);
+    bookCard.setAttribute("data-id", book.id);
+    bookCard.append(bookTitle, bookInfo, bookActions);
 
     booksDisplay.appendChild(bookCard);
   }
 }
+function resetForm() {
+  bookForm.reset();
+}
 
-addBookToLibrary("Harry Potter", "J.K Rolling");
-addBookToLibrary("Harry Potter 2", "J.K Rolling");
-addBookToLibrary("Harry Potter 3", "J.K Rolling");
-addBookToLibrary("Harry Potter 4", "J.K Rolling");
-addBookToLibrary("Harry Potter 4", "J.K Rolling");
-addBookToLibrary("Harry Potter 4", "J.K Rolling");
+function handleFormSubmit(e) {
+  e.preventDefault();
 
-const dialog = document.getElementById("book-dialog");
+  const formData = new FormData(e.target);
+
+  const newBook = {
+    name: formData.get("name"),
+    author: formData.get("author"),
+    pages: formData.get("pages"),
+    read: formData.get("read") ? true : false,
+  };
+
+  addBookToLibrary(newBook.name, newBook.author, newBook.pages, newBook.read);
+
+  dialog.close();
+  resetForm();
+}
+function handleActionButtonClick(e) {
+  const actionBtn = e.target.closest("button[data-action]");
+
+  if (!actionBtn) return;
+
+  const action = actionBtn.dataset.action;
+  const id = actionBtn.closest(".book-card").dataset.id;
+
+  switch (action) {
+    case "delete":
+      deleteBook(id);
+      break;
+
+    case "toggle-read":
+      toggleBookRead(id);
+      break;
+  }
+
+  renderBookCards();
+}
 
 document.getElementById("open-dialog").addEventListener("click", () => {
   dialog.showModal();
 });
-
 document.getElementById("cancel-btn").addEventListener("click", () => {
   dialog.close();
+  resetForm();
 });
+
+bookForm.addEventListener("submit", handleFormSubmit);
+booksDisplay.addEventListener("click", handleActionButtonClick);
